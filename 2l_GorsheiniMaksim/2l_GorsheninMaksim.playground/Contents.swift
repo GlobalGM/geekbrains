50facc77dd23c4e752bc16e11407566847e2eb95
Name Surname <username@mail.ru>
2017-12-18 16:22:35 +0300

    Скоректировал 3 задачу. И в 4 изменил предел самива до 10 значений

--- 2l_GorsheiniMaksim/2l_GorsheninMaksim.playground/Contents.swift
+++ 2l_GorsheiniMaksim/2l_GorsheninMaksim.playground/Contents.swift
@@ -6,8 +6,7 @@ import UIKit
 func t1(_ a1: Int) {
     if a1 % 2 == 0 {
             print("Число \(a1) является четным \n")
-    }
-    else {
+    } else {
             print("Число \(a1) является не четным \n")
     }
 }
@@ -20,8 +19,7 @@ t1(2)
 func t2(_ a2: Int) {
     if a2 % 3 == 0 {
         print("Число \(a2) число делится на 3 без остатка \n")
-    }
-    else {
+    } else {
         print("Число \(a2) число не делится на 3 без остатка \n")
     }
 }
@@ -33,11 +31,10 @@ t2(2)
 // Задача 3. Создать возрастающий массив из 100 чисел, при этом удалить из этого массива все четные числа и все числа, которые не делятся на 3.
 var a3 = 0
 var someArray = [Int]()
-while someArray.count < 100 {
-    if a3 % 2 == 0 || a3 % 3 == 0 {
+while a3 <= 100 {
+    if a3 % 2 == 0 || a3 % 3 != 0 {
         a3+=1
-    }
-    else {
+    } else {
         someArray.append(a3)
         a3+=1
     }
@@ -48,9 +45,7 @@ print("Массив состоит из \(someArray.count) элементов: \
 
 
 // Задача 4. Написать функцию, которая добавляет в массив новое число Фибоначчи и добавить при помощи нее 100 элементов: Числа Фибоначчи определяются соотношениями Fn=Fn-1 + Fn-2.
-
-
-
+//!!! ВНИМАНИЕ не желательно строить массив чисел из более чем 25 значений. Мошный комп может зависнуть или перегреться от вычеслений.
 func fibonacci(_ i: Int) -> Int {
     switch(i) {
     case let n where n <= 0: return 0
@@ -58,15 +53,13 @@ func fibonacci(_ i: Int) -> Int {
     case let n: return fibonacci(n - 1) + fibonacci(n - 2)
     }
 }
-
-print(fibonacci(5))
-
 var a4 = 0
 var someArrayFibonachi = [Int]()
-while someArrayFibonachi.count < 25 {
+while someArrayFibonachi.count < 10 {
         fibonacci (a4)
         someArrayFibonachi.append(fibonacci (a4))
         a4+=1
 }
 print("Массив состоит из \(someArrayFibonachi.count) чисел Фибоначи:")
 print(someArrayFibonachi)
+
